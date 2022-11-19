840969bfc190226fa5432c1a6e6b78feca9cbc9c
gagagagagagag <jakubprzywara@icloud.com>
2022-11-19 12:05:49 +0100

    Added all providers to main

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,14 +1,29 @@
 import React from 'react'
+import { SWRConfig } from 'swr'
 import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
+import { MantineProvider } from '@mantine/core'
+import { ModalsProvider } from '@mantine/modals'
+import { NotificationsProvider } from '@mantine/notifications'
+import { BrowserRouter } from 'react-router-dom'
 
 import { App } from './components/app'
 import store from './store'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <MantineProvider>
+      <Provider store={store}>
+        <ModalsProvider>
+          <NotificationsProvider>
+            <BrowserRouter>
+              <SWRConfig>
+                <App />
+              </SWRConfig>
+            </BrowserRouter>
+          </NotificationsProvider>
+        </ModalsProvider>
+      </Provider>
+    </MantineProvider>
   </React.StrictMode>
 )
