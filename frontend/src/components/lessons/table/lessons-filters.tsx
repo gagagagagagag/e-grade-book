1e057a4818c29c6ca31d04b46cf11fd5ed9e4454
gagagagagagag <jakubprzywara@icloud.com>
2022-11-25 12:40:50 +0100

    Basic admin filters

--- frontend/src/components/lessons/table/lessons-filters.tsx
+++ frontend/src/components/lessons/table/lessons-filters.tsx
@@ -1,5 +1,29 @@
+import { DateRangePicker } from '@mantine/dates'
+
+import { AllGroupsSelect } from '../../groups/all-groups-select'
+import {
+  AllStudentsSelect,
+  AllTeachersSelect,
+} from '../../users/all-users-select'
 import { UserRoles } from '../../users/types'
 
 export const LessonsFilters = ({ currentRole }: { currentRole: UserRoles }) => {
-  return <></>
+  return (
+    <>
+      <DateRangePicker
+        label={'Data'}
+        variant={'filled'}
+        allowSingleDateInRange
+      />
+      {currentRole === UserRoles.Admin && (
+        <AllTeachersSelect label={'Nauczyciel'} variant={'filled'} clearable />
+      )}
+      {currentRole === UserRoles.Admin ? (
+        <AllGroupsSelect label={'Grupa'} variant={'filled'} clearable />
+      ) : currentRole === UserRoles.Teacher ? null : null}
+      {currentRole === UserRoles.Admin ? (
+        <AllStudentsSelect label={'Uczeń'} variant={'filled'} clearable />
+      ) : currentRole === UserRoles.Student ? null : null}
+    </>
+  )
 }
